aec3609cf020cab68ebb742e5335d18982195463
Joel <joel.knoll1@gmail.com>
2025-02-15 16:33:45 -0500

    Added event queue, moved event queue higher and closer to edges of board.

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -2,59 +2,87 @@ import React from 'react';
 
 const GameBoard = () => {
   return (
-    <div className="w-full h-screen bg-gray-800 p-4">
-      <div className="w-full h-full flex justify-between">
+    <div className='w-full h-screen bg-gray-800 p-4'>
+      <div className='w-full h-full flex justify-between'>
         {/* Left Player Area */}
-        <div className="w-1/3 h-full border border-gray-600 p-2">
-        <div style={{ marginTop: '100px' }}>
-          {/* Event Queue */}
-          <div className="flex justify-between mb-8">
-            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-              <div className="text-white text-center mt-12">Event 3</div>
+        <div className='w-1/3 h-full border border-gray-600 p-2'>
+          <div
+            style={{
+              marginTop: '50px',
+            }}
+          >
+            {/* Event Queue */}
+            <div className='flex justify-start gap-2 mb-8 ml-4'>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                <div className='text-white text-center mt-12'>
+                  Event 3
+                </div>
+              </div>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                <div className='text-white text-center mt-12'>
+                  Event 2
+                </div>
+              </div>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                <div className='text-white text-center mt-12'>
+                  Event 1
+                </div>
+              </div>
             </div>
-            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-              <div className="text-white text-center mt-12">Event 2</div>
-            </div>
-            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-              <div className="text-white text-center mt-12">Event 1</div>
-            </div>
-          </div>
             {/* Three columns of cards */}
-            <div className="flex justify-between">
+            <div className='flex justify-between'>
               {/* Column 1 */}
-              <div className="flex flex-col">
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
-                  <div className="text-white text-center mt-12">Person 1</div>
+              <div className='flex flex-col'>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4'>
+                  <div className='text-white text-center mt-12'>
+                    Person 1
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-                  <div className="text-white text-center mt-12">Person 2</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8'>
+                  <div className='text-white text-center mt-12'>
+                    Person 2
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                  <div className="text-white text-center mt-12">Camp 1</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                  <div className='text-white text-center mt-12'>
+                    Camp 1
+                  </div>
                 </div>
               </div>
               {/* Column 2 */}
-              <div className="flex flex-col">
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
-                  <div className="text-white text-center mt-12">Person 3</div>
+              <div className='flex flex-col'>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4'>
+                  <div className='text-white text-center mt-12'>
+                    Person 3
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-                  <div className="text-white text-center mt-12">Person 4</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8'>
+                  <div className='text-white text-center mt-12'>
+                    Person 4
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                  <div className="text-white text-center mt-12">Camp 2</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                  <div className='text-white text-center mt-12'>
+                    Camp 2
+                  </div>
                 </div>
               </div>
               {/* Column 3 */}
-              <div className="flex flex-col">
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
-                  <div className="text-white text-center mt-12">Person 5</div>
+              <div className='flex flex-col'>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4'>
+                  <div className='text-white text-center mt-12'>
+                    Person 5
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-                  <div className="text-white text-center mt-12">Person 6</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8'>
+                  <div className='text-white text-center mt-12'>
+                    Person 6
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                  <div className="text-white text-center mt-12">Camp 3</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                  <div className='text-white text-center mt-12'>
+                    Camp 3
+                  </div>
                 </div>
               </div>
             </div>
@@ -62,29 +90,33 @@ const GameBoard = () => {
         </div>
 
         {/* Center Area */}
-        <div className="w-1/3 h-full border border-gray-600 p-2">
-          <div className="h-full flex flex-col justify-between">
+        <div className='w-1/3 h-full border border-gray-600 p-2'>
+          <div className='h-full flex flex-col justify-between'>
             {/* Top section with deck and discard */}
-            <div className="flex flex-col items-center mt-8">
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-                <div className="text-white text-center mt-12">Draw Deck</div>
+            <div className='flex flex-col items-center mt-8'>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8'>
+                <div className='text-white text-center mt-12'>
+                  Draw Deck
+                </div>
               </div>
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                <div className="text-white text-center mt-12">Discard Pile</div>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                <div className='text-white text-center mt-12'>
+                  Discard Pile
+                </div>
               </div>
-          </div>
-    
+            </div>
+
             {/* Bottom section with water counters */}
-            <div className="flex justify-between mb-8">
+            <div className='flex justify-between mb-8'>
               {/* Left player water counter */}
-              <div className="flex items-center">
-                <div className="bg-blue-600 rounded-lg p-4 text-white font-bold text-xl">
+              <div className='flex items-center'>
+                <div className='bg-blue-600 rounded-lg p-4 text-white font-bold text-xl'>
                   💧 3
                 </div>
               </div>
               {/* Right player water counter */}
-              <div className="flex items-center">
-                <div className="bg-blue-600 rounded-lg p-4 text-white font-bold text-xl">
+              <div className='flex items-center'>
+                <div className='bg-blue-600 rounded-lg p-4 text-white font-bold text-xl'>
                   💧 3
                 </div>
               </div>
@@ -93,56 +125,84 @@ const GameBoard = () => {
         </div>
 
         {/* Right Player Area */}
-        <div className="w-1/3 h-full border border-gray-600 p-2">
-        <div style={{ marginTop: '100px' }}>
-        {/* Event Queue */}
-        <div className="flex justify-between mb-8">
-          <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-            <div className="text-white text-center mt-12">Event 1</div>
-          </div>
-          <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-            <div className="text-white text-center mt-12">Event 2</div>
-          </div>
-          <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-            <div className="text-white text-center mt-12">Event 3</div>
-          </div>
-        </div>
+        <div className='w-1/3 h-full border border-gray-600 p-2'>
+          <div
+            style={{
+              marginTop: '50px',
+            }}
+          >
+            {/* Event Queue */}
+            <div className='flex justify-end gap-2 mb-8 mr-4'>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                <div className='text-white text-center mt-12'>
+                  Event 1
+                </div>
+              </div>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                <div className='text-white text-center mt-12'>
+                  Event 2
+                </div>
+              </div>
+              <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                <div className='text-white text-center mt-12'>
+                  Event 3
+                </div>
+              </div>
+            </div>
             {/* Three columns of cards */}
-            <div className="flex justify-between">
+            <div className='flex justify-between'>
               {/* Column 1 */}
-              <div className="flex flex-col">
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
-                  <div className="text-white text-center mt-12">Person 1</div>
+              <div className='flex flex-col'>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4'>
+                  <div className='text-white text-center mt-12'>
+                    Person 1
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-                  <div className="text-white text-center mt-12">Person 2</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8'>
+                  <div className='text-white text-center mt-12'>
+                    Person 2
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                  <div className="text-white text-center mt-12">Camp 1</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                  <div className='text-white text-center mt-12'>
+                    Camp 1
+                  </div>
                 </div>
               </div>
               {/* Column 2 */}
-              <div className="flex flex-col">
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
-                  <div className="text-white text-center mt-12">Person 3</div>
+              <div className='flex flex-col'>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4'>
+                  <div className='text-white text-center mt-12'>
+                    Person 3
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-                  <div className="text-white text-center mt-12">Person 4</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8'>
+                  <div className='text-white text-center mt-12'>
+                    Person 4
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                  <div className="text-white text-center mt-12">Camp 2</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                  <div className='text-white text-center mt-12'>
+                    Camp 2
+                  </div>
                 </div>
               </div>
               {/* Column 3 */}
-              <div className="flex flex-col">
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
-                  <div className="text-white text-center mt-12">Person 5</div>
+              <div className='flex flex-col'>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4'>
+                  <div className='text-white text-center mt-12'>
+                    Person 5
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-                  <div className="text-white text-center mt-12">Person 6</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8'>
+                  <div className='text-white text-center mt-12'>
+                    Person 6
+                  </div>
                 </div>
-                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                  <div className="text-white text-center mt-12">Camp 3</div>
+                <div className='w-24 h-32 border-2 border-gray-400 rounded bg-gray-700'>
+                  <div className='text-white text-center mt-12'>
+                    Camp 3
+                  </div>
                 </div>
               </div>
             </div>
