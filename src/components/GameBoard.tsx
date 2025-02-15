d9fb1011e6c4f6c782c5cf6879ac86c833e79262
Joel <joel.knoll1@gmail.com>
2025-02-15 16:25:36 -0500

    Added water counters

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -63,12 +63,31 @@ const GameBoard = () => {
 
         {/* Center Area */}
         <div className="w-1/3 h-full border border-gray-600 p-2">
-          <div className="flex flex-col items-center justify-center h-full">
-            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
-              <div className="text-white text-center mt-12">Draw Deck</div>
-            </div>
-            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-              <div className="text-white text-center mt-12">Discard Pile</div>
+          <div className="h-full flex flex-col justify-between">
+            {/* Top section with deck and discard */}
+            <div className="flex flex-col items-center mt-8">
+              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
+                <div className="text-white text-center mt-12">Draw Deck</div>
+              </div>
+              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+                <div className="text-white text-center mt-12">Discard Pile</div>
+              </div>
+          </div>
+    
+            {/* Bottom section with water counters */}
+            <div className="flex justify-between mb-8">
+              {/* Left player water counter */}
+              <div className="flex items-center">
+                <div className="bg-blue-600 rounded-lg p-4 text-white font-bold text-xl">
+                  💧 3
+                </div>
+              </div>
+              {/* Right player water counter */}
+              <div className="flex items-center">
+                <div className="bg-blue-600 rounded-lg p-4 text-white font-bold text-xl">
+                  💧 3
+                </div>
+              </div>
             </div>
           </div>
         </div>
