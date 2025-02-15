a5ede98913f024b916c7a7d24ceedf40a021a1f6
Joel <joel.knoll1@gmail.com>
2025-02-15 16:22:22 -0500

    Added person card tableaus and event queues.

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -6,17 +6,56 @@ const GameBoard = () => {
       <div className="w-full h-full flex justify-between">
         {/* Left Player Area */}
         <div className="w-1/3 h-full border border-gray-600 p-2">
-          <div style={{ marginTop: '400px' }}>
-            <div className="text-white mb-4">Left Player Camps</div>
+        <div style={{ marginTop: '100px' }}>
+          {/* Event Queue */}
+          <div className="flex justify-between mb-8">
+            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+              <div className="text-white text-center mt-12">Event 3</div>
+            </div>
+            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+              <div className="text-white text-center mt-12">Event 2</div>
+            </div>
+            <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+              <div className="text-white text-center mt-12">Event 1</div>
+            </div>
+          </div>
+            {/* Three columns of cards */}
             <div className="flex justify-between">
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                <div className="text-white text-center mt-12">Camp 1</div>
+              {/* Column 1 */}
+              <div className="flex flex-col">
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
+                  <div className="text-white text-center mt-12">Person 1</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
+                  <div className="text-white text-center mt-12">Person 2</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+                  <div className="text-white text-center mt-12">Camp 1</div>
+                </div>
               </div>
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                <div className="text-white text-center mt-12">Camp 2</div>
+              {/* Column 2 */}
+              <div className="flex flex-col">
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
+                  <div className="text-white text-center mt-12">Person 3</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
+                  <div className="text-white text-center mt-12">Person 4</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+                  <div className="text-white text-center mt-12">Camp 2</div>
+                </div>
               </div>
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                <div className="text-white text-center mt-12">Camp 3</div>
+              {/* Column 3 */}
+              <div className="flex flex-col">
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
+                  <div className="text-white text-center mt-12">Person 5</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
+                  <div className="text-white text-center mt-12">Person 6</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+                  <div className="text-white text-center mt-12">Camp 3</div>
+                </div>
               </div>
             </div>
           </div>
@@ -36,17 +75,56 @@ const GameBoard = () => {
 
         {/* Right Player Area */}
         <div className="w-1/3 h-full border border-gray-600 p-2">
-          <div style={{ marginTop: '400px' }}>
-            <div className="text-white mb-4">Right Player Camps</div>
+        <div style={{ marginTop: '100px' }}>
+        {/* Event Queue */}
+        <div className="flex justify-between mb-8">
+          <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+            <div className="text-white text-center mt-12">Event 1</div>
+          </div>
+          <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+            <div className="text-white text-center mt-12">Event 2</div>
+          </div>
+          <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+            <div className="text-white text-center mt-12">Event 3</div>
+          </div>
+        </div>
+            {/* Three columns of cards */}
             <div className="flex justify-between">
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                <div className="text-white text-center mt-12">Camp 1</div>
+              {/* Column 1 */}
+              <div className="flex flex-col">
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
+                  <div className="text-white text-center mt-12">Person 1</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
+                  <div className="text-white text-center mt-12">Person 2</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+                  <div className="text-white text-center mt-12">Camp 1</div>
+                </div>
               </div>
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                <div className="text-white text-center mt-12">Camp 2</div>
+              {/* Column 2 */}
+              <div className="flex flex-col">
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
+                  <div className="text-white text-center mt-12">Person 3</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
+                  <div className="text-white text-center mt-12">Person 4</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+                  <div className="text-white text-center mt-12">Camp 2</div>
+                </div>
               </div>
-              <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
-                <div className="text-white text-center mt-12">Camp 3</div>
+              {/* Column 3 */}
+              <div className="flex flex-col">
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4">
+                  <div className="text-white text-center mt-12">Person 5</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-8">
+                  <div className="text-white text-center mt-12">Person 6</div>
+                </div>
+                <div className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700">
+                  <div className="text-white text-center mt-12">Camp 3</div>
+                </div>
               </div>
             </div>
           </div>
