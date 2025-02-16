15b2816232432ab29a851a298a20f68597ce8b48
Joel <joel.knoll1@gmail.com>
2025-02-16 00:39:26 -0500

    created personslot.tsx as a reusable component for Person slots.

---
+++ src/components/PersonSlot.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React from 'react';
+import { Card } from '@/types/game';
+
+interface PersonSlotProps {
+  index: number;
+  card: Card | null;
+  setPersonSlots: React.Dispatch<React.SetStateAction<(Card | null)[]>>;
+  handCards: Card[];
+  setHandCards: React.Dispatch<React.SetStateAction<Card[]>>;
+}
+
+const PersonSlot = ({ index, card, setPersonSlots, handCards, setHandCards }: PersonSlotProps) => {
+  return (
+    <div
+      className="w-24 h-32 border-2 border-gray-400 rounded bg-gray-700 mb-4"
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        e.preventDefault();
+        const cardId = e.dataTransfer.getData('cardId');
+        const draggedCard = handCards.find((card) => card.id === cardId);
+
+        if (draggedCard) {
+          const newSlots = [...Array(2)].map((_, i) => (i === index ? draggedCard : null));
+          setPersonSlots(newSlots);
+          setHandCards(handCards.filter((card) => card.id !== cardId));
+        }
+      }}
+    >
+      {card ? (
+        <div className="text-white text-center text-xs mt-4">
+          {card.name}
+          <br />
+          {card.type}
+          <br />
+          {card.id}
+        </div>
+      ) : (
+        <div className="text-white text-center mt-12">Person {index + 1}</div>
+      )}
+    </div>
+  );
+};
+
+export default PersonSlot;
